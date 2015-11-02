00d3b70c6bf78e757bdac8a502bd01d7c555db89
erwinvandervalk <erwin@vandervalk.pro>
2015-11-02 20:25:12 +0100

    Cleans up camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,34 +3,17 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public float interpVelocity;
-    public float minDistance;
-    public float followDistance;
     public GameObject target;
-    public Vector3 offset;
-    Vector3 targetPos;
-    // Use this for initialization
-    void Start()
-    {
-        targetPos = transform.position;
-    }
+    public float minX;
+    public float maxX;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (target)
         {
-            Vector3 posNoZ = transform.position;
-            posNoZ.z = target.transform.position.z;
-
-            Vector3 targetDirection = (target.transform.position - posNoZ);
-
-            interpVelocity = targetDirection.magnitude * 5f;
-
-            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
-
-            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
-
+            var x = Mathf.Max(Mathf.Min(maxX, target.transform.position.x), minX);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
     }
 }
